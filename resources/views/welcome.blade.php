a0feda0f5f400c9d6f92d325708d5cafd9655b1d
visnicio <vinicius.antonio0712@gmail.com>
2023-02-08 23:06:55 -0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Alecrim Dourado</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        @vite('resources/css/app.css')
+
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body class="antialiased bg-gray-900">
+        <nav class="p-4 bg-purple-600">
+            <div class="container">
+                <h1 class="font-bold text-white text-2xl">Alecrim Dourado</h1>
+            </div>
+        </nav>
+    </body>
+</html>
